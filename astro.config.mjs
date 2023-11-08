18087e5f0fe37f6cfc850ba720731290ffc14be8
Adrinc <abrahamd@cbluna.com>
2023-11-07 16:43:02 -0800

    uwifisvg  añadido, yam añadido

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://uwif_astro_web.github.io',
   integrations: [tailwind()]
 });
