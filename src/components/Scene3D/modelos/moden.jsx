f275c53934b3a917e21e43d96be5a9f08b0f8acd
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2023-11-08 13:55:35 -0800

    new moden

--- src/components/Scene3D/modelos/moden.jsx
+++ src/components/Scene3D/modelos/moden.jsx
@@ -23,100 +23,97 @@ export default function Moden(props) {
           receiveShadow
           geometry={nodes.Logo_luz.geometry}
           material={materials.Material}
-          position={[0.8449, 0.4949, -0.0299]}
-          scale={0.9316}
+          position={[0.845, 0.495, -0.03]}
+          scale={0.932}
         />
         <group
           name="Cuerpo"
-          position={[0.0072, 0.0599, 0.0047]}
-          scale={[0.8419, 0.7523, 0.5009]}
-          castShadow
-          receiveShadow
+          position={[0.392, 0.642, -0.034]}
+          scale={[0.012, 0.033, 0.012]}
         >
           <mesh
-            name="Plano002"
+            name="Cubo015"
             castShadow
             receiveShadow
-            geometry={nodes.Plano002.geometry}
-            material={materials.white}
-            
-          />
-          <mesh
-            name="Plano002_1"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_1.geometry}
-            material={materials.yellow}
-          />
-          <mesh
-            name="Plano002_2"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_2.geometry}
-            material={materials.Upurple}
-          />
-          <mesh
-            name="Plano002_3"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_3.geometry}
-            material={materials.black}
-          />
-          <mesh
-            name="Plano002_4"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_4.geometry}
-            material={materials.Bronce}
-          />
-          <mesh
-            name="Plano002_5"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_5.geometry}
-            material={materials.gray}
-          />
-          <mesh
-            name="Plano002_6"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_6.geometry}
-            material={materials["Material.004"]}
-          />
-          <mesh
-            name="Plano002_7"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_7.geometry}
-            material={materials.Plata}
-          />
-          <mesh
-            name="Plano002_8"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_8.geometry}
-            material={materials.BotonON}
-          />
-          <mesh
-            name="Plano002_9"
-            castShadow
-            receiveShadow
-            geometry={nodes.Plano002_9.geometry}
+            geometry={nodes.Cubo015.geometry}
             material={materials.Material}
           />
           <mesh
-            name="Plano002_10"
+            name="Cubo015_1"
             castShadow
             receiveShadow
-            geometry={nodes.Plano002_10.geometry}
-            material={materials["Material.001"]}
+            geometry={nodes.Cubo015_1.geometry}
+            material={materials.white}
           />
           <mesh
-            name="Plano002_11"
+            name="Cubo015_2"
             castShadow
             receiveShadow
-            geometry={nodes.Plano002_11.geometry}
+            geometry={nodes.Cubo015_2.geometry}
             material={materials.blackCristal}
+          />
+          <mesh
+            name="Cubo015_3"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_3.geometry}
+            material={materials.gray}
+          />
+          <mesh
+            name="Cubo015_4"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_4.geometry}
+            material={materials.yellow}
+          />
+          <mesh
+            name="Cubo015_5"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_5.geometry}
+            material={materials.Upurple}
+          />
+          <mesh
+            name="Cubo015_6"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_6.geometry}
+            material={materials.black}
+          />
+          <mesh
+            name="Cubo015_7"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_7.geometry}
+            material={materials.Bronce}
+          />
+          <mesh
+            name="Cubo015_8"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_8.geometry}
+            material={materials["Material.004"]}
+          />
+          <mesh
+            name="Cubo015_9"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_9.geometry}
+            material={materials.Plata}
+          />
+          <mesh
+            name="Cubo015_10"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_10.geometry}
+            material={materials.BotonON}
+          />
+          <mesh
+            name="Cubo015_11"
+            castShadow
+            receiveShadow
+            geometry={nodes.Cubo015_11.geometry}
+            material={materials["Material.001"]}
           />
         </group>
       </group>
