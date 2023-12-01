8f3e89f98c9b92049147b39a073cd49ce0f0fafb
Adrinc <abrahamd@cbluna.com>
2023-12-01 12:49:43 -0800

    cambios demo wip responsive sec 2 y 3

--- src/components/react_components/TopMenu.jsx
+++ src/components/react_components/TopMenu.jsx
@@ -5,13 +5,23 @@ const TopMenu = () => {
     const [showMenu, setShowMenu] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
 
+    const links = [
+        { name: 'HOME', url: '#home', selected: true },
+        { name: 'ACP', url: '#acp',selected: false  },
+        { name: 'PLAN', url: '#plan' ,selected: false  },
+        { name: 'HOW?', url: '#howit' ,selected: false  },
+        { name: 'APP', url: '#app' ,selected: false  },
+        { name: '5G', url: '#5g' ,selected: false  },
+      ];
+      
+
     const toggleMenu = () => {
         console.log('toggling menu');
         setShowMenu(!showMenu);
     };
 
     const handleResize = () => {
-        setIsMobile(window.innerWidth <= 550);
+        setIsMobile(window.innerWidth <= 768);
     };
 
     useEffect(() => {
@@ -35,36 +45,21 @@ const TopMenu = () => {
 
                 {isMobile && (
                     <div className={TopMenuStyle.hamburgerIcon} onClick={toggleMenu}>
-                        &#9776;
+                        &#9fff76;
                     </div>
                 )}
 
                 <nav className={`${TopMenuStyle.navigation} ${isMobile && showMenu ? TopMenuStyle.showMenu : ''}`}>
-                    <a href="#" className={TopMenuStyle['selected-link']}>
-                        HOME
+                    {links.map(link => (
+                    <a href={link.url} className={link.selected ? TopMenuStyle['selected-link'] : TopMenuStyle.link}>
+                        {link.name}
                     </a>
-                    <a href="#" className={TopMenuStyle.link}>
-                        PLANS
-                    </a>
-                    <a href="#" className={TopMenuStyle.link}>
-                        DEVICES
-                    </a>
-                    <a href="#" className={TopMenuStyle.link}>
-                        DEALS
-                    </a>
-                    <a href="#" className={TopMenuStyle.link}>
-                        COVERAGE
-                    </a>
-                    <a href="#" className={TopMenuStyle.link}>
-                        FAQ
-                    </a>
+                    ))}
                 </nav>
 
-                <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/19775a3f-e559-4fca-9726-f6a6a94313b8?apiKey=99b0eaa04efc41a78ff2850239e1f525&"
-                    className={TopMenuStyle.image2}
-                />
+                <div class="hidden xl:block xl:w-1/3">
+          <div class="flex items-center justify-end"><a class="inline-block buttom-shadow py-2 px-4 text-xl font-bold leading-5 text-green-50 buttom-text-shadow drop-shadow-sm bg-green-500  hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md" href="#">Login</a></div>
+        </div>
             </div>
         </>
     );
