12280131c590164afd779d79b3b57089cd92622e
Adrinc <abrahamd@cbluna.com>
2023-12-05 13:02:57 -0800

    cambios en navbar

---
+++ src/components/react_components/RouterLinks.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import TopMenu from './TopMenu';
+
+function RoterLinks() {
+  return (
+    <BrowserRouter>
+      <TopMenu />
+    </BrowserRouter>
+  );
+}
+
+export default RoterLinks;
