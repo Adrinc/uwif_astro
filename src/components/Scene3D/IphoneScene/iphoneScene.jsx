797ffaed79453d8d0e4e33dcd51ce6d822643515
Adrinc <abrahamd@cbluna.com>
2024-01-09 15:36:16 -0800

    save before pull

--- src/components/Scene3D/IphoneScene/iphoneScene.jsx
+++ src/components/Scene3D/IphoneScene/iphoneScene.jsx
@@ -1,7 +1,7 @@
-import {Environment, PerspectiveCamera, PresentationControls, Plane,Box, OrbitControls} from '@react-three/drei'
+import {Environment, PerspectiveCamera, PresentationControls, Box,  MeshReflectorMaterial, Float} from '@react-three/drei'
 import { Suspense } from 'react'
-import { ToneMapping , ChromaticAberration, Bloom, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
-import { enviromentConfig, pointLightConfig } from '../scene_config.js';
+import { ToneMapping,  Bloom, EffectComposer} from '@react-three/postprocessing'
+import { enviromentConfig} from '../scene_config.js';
 
 import Iphone from './iphone.jsx';
 import { BlendFunction, Resolution } from 'postprocessing';
@@ -9,7 +9,7 @@ import { BlendFunction, Resolution } from 'postprocessing';
 import * as THREE from 'three'
 export default function IphoneScene()
 {
-    let initCamera={far:2000, near: 0.1, fov: 45, position:[0.3, 4.2,10.2], rotation:[0, 0, 0]}
+    let initCamera={far:2000, near: 0.1, fov: 45, position:[0.3, 4.2,11], rotation:[0, 0, 0]}
     const bloomIphone = {
         mipmapBlur: true,
         intensity: 1,
@@ -37,8 +37,8 @@ export default function IphoneScene()
     return <> 
 
 <Suspense fallback={null}>
-    <color attach="background" args={['#000']} />
-    <fog attach="fog" args={['#ffff', 5, 50]} />
+    <color attach="background" args={['#000000']} />
+    <fog attach="fog" args={['#341768', 10, 50]} />
         <EffectComposer multisampling={4}>
             <ToneMapping {...toneMappingConfig}/>
             <Bloom {...bloomIphone} />
@@ -47,26 +47,59 @@ export default function IphoneScene()
 
     
     <PerspectiveCamera makeDefault  {...initCamera}/>
+ 
     <Environment {...enviromentConfig}/>
     <pointLight {...pointLightConfigs} shadow-bias={-0.0003} shadow-mapSize={ [ 512*6,512*6] } shadow-normalBias={0.01}/>
     <ambientLight intensity={3.2} color={"#5d5d5d"} />
     <Suspense>
-{/*     <Iphone  position={[-0.5,0,0]} /> */}
 
-    <Box args={[100,100,50]} rotation={[-Math.PI/2,0,0]} position={[0,25,0]} receiveShadow   castShadow>
-        <meshStandardMaterial side={THREE.DoubleSide} color={"#ffffff"} />
-    </Box>
+
+  
+
+
 
     <PresentationControls
      global={true}
      cursor={true}
+     
     config={{ mass: 2, tension: 500 }}
     snap={{ mass: 4, tension: 1500 }}
     rotation={[0,-1.56,0]}
     polar={[-Math.PI / 3, Math.PI / 3]}
     azimuth={[-Math.PI / 1.4, Math.PI / 2]}>
 
+<Float
+  speed={3} // Animation speed, defaults to 1
+  rotationIntensity={0.1} // XYZ rotation intensity, defaults to 1
+  floatIntensity={1} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+  floatingRange={[-0.1, 0.3]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+>
+ 
     <Iphone  position={[-0.5,0,0]} />
+</Float>
+
+    <mesh rotation={[-Math.PI / 2, 0, 0]}>
+
+    <planeGeometry args={[100, 100]} />
+  <MeshReflectorMaterial
+     blur={[300, 100]}
+     resolution={2048}
+     mixBlur={1}
+     mixStrength={80}
+     roughness={1}
+     depthScale={1.2}
+     minDepthThreshold={0.4}
+     maxDepthThreshold={1.4}
+     color="#0A0A0A"
+     metalness={0.5}
+  />
+    
+</mesh>
+<Box args={[100,100,50]} rotation={[-Math.PI/2,0,0]} position={[0,2,0]} receiveShadow   castShadow> 
+
+<meshStandardMaterial side={THREE.DoubleSide} color={"#000000"} />
+
+</Box>
     </PresentationControls>
     </Suspense>
 
