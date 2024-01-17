15baf151a96b4fc7464f5e4ee984199af916d515
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2024-01-16 16:56:18 -0800

    aun más cambios en imagenes en navbar

--- src/components/react_components/TopMenu.jsx
+++ src/components/react_components/TopMenu.jsx
@@ -96,7 +96,7 @@ const TopMenu = () => {
                 <div className="md:hidden flex flex-col gap-1 absolute justify-evenly top-0 left-0 w-[50vw] h-[100vh] shadow-lg z-50 bg-[#00000070] backdrop-filter backdrop-blur-lg border-x-2 border-tertiaryColor animate-fade-right animate-duration-500">
                     <div className='flex flex-row justify-end p-4 '>
               
-                        <button onClick={toggleMenu} className=" border-2 rounded-lg text-primaryTextColor p-2  font-bold text-lg">
+                        <button aria-label="burger buttun close" onClick={toggleMenu} className=" border-2 rounded-lg text-primaryTextColor p-2  font-bold text-lg">
                             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill='#ffffff' fillRule="evenodd" clipRule="evenodd">
                                 <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"/>
                             </svg>
@@ -144,13 +144,14 @@ const TopMenu = () => {
                  
                     src="/image/uwifilogo.svg"
                     alt='uwifi logo'
+                    width="100" height="50"
                     className={TopMenuStyle.image}
                 />
                 <span className={TopMenuStyle.description}><span className={TopMenuStyle.boltext}>CALL NOW! </span> (123)456-789-000</span>
          
 
                 {isMobile && (
-                        <button className="navbar-burger self-center md:hidden" onClick={toggleMenu}>
+                        <button aria-label="Burger button" className="navbar-burger self-center md:hidden" onClick={toggleMenu}>
                         <svg width="35" height="35" viewBox="0 0 32 32" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
                           <rect className="text-coolGray-50" width="32" height="32" rx="6" fill="#ffffff"></rect>
                           <path className="text-coolGray-500" d="M7 12H25C25.2652 12 25.5196 11.8946 25.7071 11.7071C25.8946 11.5196 26 11.2652 26 11C26 10.7348 25.8946 10.4804 25.7071 10.2929C25.5196 10.1054 25.2652 10 25 10H7C6.73478 10 6.48043 10.1054 6.29289 10.2929C6.10536 10.4804 6 10.7348 6 11C6 11.2652 6.10536 11.5196 6.29289 11.7071C6.48043 11.8946 6.73478 12 7 12ZM25 15H7C6.73478 15 6.48043 15.1054 6.29289 15.2929C6.10536 15.4804 6 15.7348 6 16C6 16.2652 6.10536 16.5196 6.29289 16.7071C6.48043 16.8946 6.73478 17 7 17H25C25.2652 17 25.5196 16.8946 25.7071 16.7071C25.8946 16.5196 26 16.2652 26 16C26 15.7348 25.8946 15.4804 25.7071 15.2929C25.5196 15.1054 25.2652 15 25 15ZM25 20H7C6.73478 20 6.48043 20.1054 6.29289 20.2929C6.10536 20.4804 6 20.7348 6 21C6 21.2652 6.10536 21.5196 6.29289 21.7071C6.48043 21.8946 6.73478 22 7 22H25C25.2652 22 25.5196 21.8946 25.7071 21.7071C25.8946 21.5196 26 21.2652 26 21C26 20.7348 25.8946 20.4804 25.7071 20.2929C25.5196 20.1054 25.2652 20 25 20Z" fill="currentColor"></path>
