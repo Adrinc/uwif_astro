e492c2a107eca39047ac0a8d824964be2504bee7
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2024-02-06 15:50:54 -0800

    dropmenu añadido a pagina faq

---
+++ src/components/react_components/DropMenu.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import DropMenuStyle from './DropMenu.module.css';
+
+const DropMenu = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen(!isOpen);
+
+  return (
+    <div className='mb-20'>
+      <button className={DropMenuStyle.dropbutton} onClick={toggleOpen}>
+          <div className='flex w-full justify-between items-center align-middle gap-4 '>
+
+              <p class={DropMenuStyle.question}>{question} </p>
+
+              <div className={isOpen ? "transform rotate-90" : ""}> 
+                <svg alt-arial="button icon"  width="32" height="32" viewBox="0 0 31 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="chevron-right 1">
+                <path id="Vector" d="M11.6987 23.5705L19.4126 15.8566L11.6987 8.1427" stroke={"#ffff"} stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                </svg>
+              </div>
+
+          </div>
+      </button>
+      {isOpen && <p className={DropMenuStyle.answer}>{answer}</p>}
+    </div>
+  );
+};
+
+export default DropMenu;
