7d29c725b3dc559cfb616bf38d395ebb14e0535f
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2024-02-21 18:09:09 -0800

    test rive link

--- src/components/react_components/RiveAnimations/antenas/antenas.jsx
+++ src/components/react_components/RiveAnimations/antenas/antenas.jsx
@@ -4,7 +4,7 @@ import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
 export const RiveDemo = () => {
   const { RiveComponent } = useRive({
     // Load a local riv `clean_the_car.riv` or upload your own!
-    src: "rive/antenas.riv",
+    src: "/rive/antenas.riv",
     // Be sure to specify the correct state machine (or animation) name
     stateMachines: "Motion",
     // This is optional.Provides additional layout control.
