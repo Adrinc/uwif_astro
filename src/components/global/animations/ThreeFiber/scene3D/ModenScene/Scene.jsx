9ed8006115dcd9feed2652070bbb6e9ddf751126
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2024-04-18 13:59:00 -0700

    cambio en antenas,  movimiento automatico en hover en moden 3d

--- src/components/global/animations/ThreeFiber/scene3D/ModenScene/Scene.jsx
+++ src/components/global/animations/ThreeFiber/scene3D/ModenScene/Scene.jsx
@@ -1,16 +1,49 @@
-import {Sparkles,  BakeShadows, Environment, OrbitControls, PerspectiveCamera, PresentationControls} from '@react-three/drei'
+import {Sparkles,  BakeShadows, Environment, PerspectiveCamera, PresentationControls} from '@react-three/drei'
 import { Suspense } from 'react'
 import { useControls } from 'leva';
 
-import { ToneMapping , ChromaticAberration, Bloom, EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+import { ToneMapping , ChromaticAberration, Bloom, EffectComposer } from '@react-three/postprocessing'
 import { enviromentConfig, pointLightConfig } from '../scene_config.js';
-import { vignetteConfig, chromaticAberrationConfig, bloomConfig, noiseConfig, toneMappingConfig } from '../scene_config.js';
+import { vignetteConfig, chromaticAberrationConfig, bloomConfig, toneMappingConfig } from '../scene_config.js';
 import Moden from './moden.jsx';
 
+import { useThree, useFrame } from '@react-three/fiber';
 export default function Scene()
 {
-    let initCamera={far:2000, near: 0.1, fov: 35, position:[0, 0.5,3], rotation:[0, 0, 0]}
+    let initCamera={far:2000, near: 0.1, fov: 35, position:[0, 1,3], rotation:[0, 0, 0]}
 
+    const { viewport } = useThree();
+  
+    let x = 0;
+    let y = 0;
+    let lastMouseX = 0;
+    let lastMouseY = 0;
+
+
+    const minPolarAngle = Math.PI / 4; 
+const maxPolarAngle = Math.PI / 2; 
+const minAzimuthAngle = -Math.PI / 4;
+const maxAzimuthAngle = Math.PI / 4; 
+
+        useFrame(({ pointer, camera, scene }) => {
+           x = (pointer.x / viewport.width) * 2 - 1;
+           y = -(pointer.y / viewport.height) * 2 + 1;
+
+           const polarAngle = (maxPolarAngle - minPolarAngle) * pointer.y + minPolarAngle;
+           const azimuthAngle = (maxAzimuthAngle - minAzimuthAngle) * pointer.x + minAzimuthAngle;
+         
+           const radius = 2.5;
+           const offsetX = radius * Math.sin(polarAngle) * Math.cos(azimuthAngle);
+           const offsetY = radius * Math.cos(polarAngle);
+           const lerpFactor = 1;
+           camera.position.x += (offsetX * 0.1 - camera.position.x) * lerpFactor;
+           camera.position.y += (offsetY * 0.1 - camera.position.y) * lerpFactor;
+         
+           camera.lookAt(scene.position);
+           lastMouseX = pointer.x;
+           lastMouseY = pointer.y;
+        })
+  
     return <> 
 
     <Suspense fallback={null}>
@@ -25,14 +58,14 @@ export default function Scene()
         </EffectComposer>
     </Suspense>
  
-    <PerspectiveCamera makeDefault  {...initCamera}/>
+    <PerspectiveCamera  makeDefault {...initCamera}/>
     <Environment {...enviromentConfig}/>
     <pointLight {...pointLightConfig} shadow-bias={-0.0003} shadow-mapSize={ [ 512*6,512*6] } shadow-normalBias={0.01}/>
     <ambientLight intensity={9.2} color={"#5d5d5d"} />
     <Suspense>
 
-
     <PresentationControls
+  
     config={{ mass: 2, tension: 500 }}
     snap={{ mass: 4, tension: 1500 }}
     rotation={[0,0,0]}
@@ -40,8 +73,9 @@ export default function Scene()
     azimuth={[-Math.PI / 1.4, Math.PI / 2]}
 
 >
-<Moden  position={[0,0,0]} rotation={[0,-1.58,0]}  />
+<Moden  position={[0,-0.5,0]} rotation={[0,-1.58,0]}  />
 </PresentationControls>
+
 
 
     
