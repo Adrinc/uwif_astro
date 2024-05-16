64e653a577c5ce4a2b11441f9435421fc4a8c24d
lynatical <carolinaroblero8@gmail.com>
2024-05-16 11:41:59 -0700

    Buttons correction

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors');
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
